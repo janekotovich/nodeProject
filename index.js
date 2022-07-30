5bfcbd01c38eeadb594ebb02eaf7713940259863
Jane-Kotovich <jenyakotovich@gmail.com>
2022-07-30 14:51:52 +1000

    Using fs to read and write

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const hello = "Hello there!";
-console.log(hello);
+const fs = require("fs");
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+
+const textOut = `This is what we know about the avocado: ${textIn}\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File has been written!");
