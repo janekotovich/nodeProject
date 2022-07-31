caec1aadfc6ba867d6c39b8d71be32ac3560b7ed
Jane-Kotovich <jenyakotovich@gmail.com>
2022-07-31 14:54:07 +1000

    Html templating part 2

--- index.js
+++ index.js
@@ -34,15 +34,55 @@ const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 ///////////////
 // SERVER
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
+  return output;
+};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
 
 const server = http.createServer((req, res) => {
   const path = req.url;
 
+  // Overview - home page
   if (path === "/overview" || path === "/") {
-    res.end("This is an overview");
-  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHtml = dataObject
+      .map((el) => replaceTemplate(tempCard, el))
+      .concat();
+    const output = tempOverview.replace("{%PRODUCT_CARDS}", cardsHtml);
+    res.end(output);
+  }
+  //EACH of the single product
+  else if (path === "/api") {
     res.writeHead(200, {
       "Content-type": "application/JSON",
     });
